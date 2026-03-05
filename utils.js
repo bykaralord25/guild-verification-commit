2e3605fe42faa6857fa0d64f73f826992453fc97
David Freeman <129763069+bykaralord25@users.noreply.github.com>
2026-03-06 00:56:10 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:48:20.749Z
-Commit: Add dark mode support
+Timestamp: 2026-03-05T21:56:08.243Z
+Commit: Resolve null pointer exception in user service
