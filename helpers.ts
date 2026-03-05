c35bc82b044d72c160b91fe462cc0299dd233def
David Freeman <129763069+bykaralord25@users.noreply.github.com>
2026-03-06 00:56:05 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:53:47.536Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T21:56:03.641Z
+Commit: Resolve null pointer exception in user service
